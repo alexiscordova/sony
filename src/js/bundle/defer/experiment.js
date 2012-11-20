ab04ff1b7c34118b0e3affa2a136aea8308d6947
jp.couture <jean-philippe.couture@nurun.com>
2012-11-20 10:15:38 -0800

    Work-in-Progress (Specs)

--- src/js/bundle/defer/experiment.js
+++ src/js/bundle/defer/experiment.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 	});
 });
 
-
 $(window).load(function()
 {
 	//init.
@@ -15,6 +14,7 @@ $(window).load(function()
     delay(function(){resize();}, 10);
 	});
 	
+	//When you resize.
 	function resize()
 	{
 		var windowWidth = $(window).width();
@@ -22,11 +22,10 @@ $(window).load(function()
 		// when the window is less or equal to 640px wide.
 		if (windowWidth <= 640)
 		{
-			
 			if ($('.table .column.show').length <= 0)
 			{
 				//We need to add the class for each column
-				$('.table tbody .row').each(function()
+				$('.table .tbody .row').each(function()
 				{
 					$(this).children('.column:nth-child(2)').addClass('show');
 				})
@@ -36,55 +35,77 @@ $(window).load(function()
 });
 
 
-//Apply when a table appear.
-if ($('table').length > 0)
+//Apply when a table appear && IE7 is not the browser.
+if ($('table').length > 0 && $.browser.version != 7)
 {
-	// counts total number of td in a head so that we can can use it for label extraction
-	var head_col_count =  $('thead th').size();
-
-	for ( i=0; i <= head_col_count; i++ )
-	{	
-		var head_col_label = $('thead th:nth-child('+ i +')').text();
+	$('table').each(function()
+	{
+		//init.
+		$table = $(this);
 		
-		$('tr td:nth-child('+ i +')').replaceWith(
+		var head_col_count = $table.find('thead th').size();
+		for ( i=0; i <= head_col_count; i++ )
+		{
+			var head_col_label = $table.find('thead th:nth-child('+ i +')').text();
+			
+			$table.find('tr td:nth-child('+ i +')').replaceWith(
 				function(){
 						return $('<div class="column" data-label="'+ head_col_label +'">').append($(this).contents());
 				}
-		);
-	}
-	var first_column_count =  $('tbody tr th:first-child').size();
-	for ( i=1; i <= (first_column_count) ; i++ )
-	{
-		var get_title = $('tbody tr:nth-child('+ i +') th:first-child').text();
+			);
+		}
 		
-		j=2;
-		$('tbody tr').each(function(){
-			$('tbody tr:nth-child('+i+') .column:nth-child('+j+')').prepend('<h2>'+get_title+'</h2>');
-			j++;
-		});
-	}
-
-	// replaces table with <div class="table">
-	$('table').replaceWith(
+		//Spread the table of title (hidden)
+		var first_column_count =  $table.find('tbody tr th:first-child').size();
+		for ( i=1; i <= (first_column_count) ; i++ )
+		{
+			var get_title = $table.find('tbody tr:nth-child('+ i +') th:first-child').text();
+			
+			j=2;
+			$table.find('tbody tr').each(function(){
+				$table.find('tbody tr:nth-child('+i+') .column:nth-child('+j+')').prepend('<h2>'+get_title+'</h2>');
+				j++;
+			});
+		}
+		
+		// replaces table with <div class="table"> && add class "bigTable" when needed else it's considered as a small Table
+		$(this).replaceWith(
 			function(){
-					return $('<div class="table">').append($(this).contents());
+				var tableKind = 'smallTable';
+				if ($table.hasClass('bigTable')){tableKind = 'bigTable';}
+					return $('<div class="table '+tableKind+' rTable">').append($(this).contents());
+			}
+		);
+	});
+	
+	//Mobile navigation
+	/*
+	var $myUl = $('<ul class="table-mobile-tabs clearfix">');
+	$('table.bigTable').each(function()
+	{
+		$(this).find('thead th').each(function()
+		{
+			$myUl.append('<li>'+$(this).text()+'</li>');
+		});
+		
+		$(this).find("li:first-child").remove(); 
+		$(this).before($myUl);
+	});
+	*/
+	
+	// replaces thead with <div class="thead">
+	$('thead').replaceWith(
+			function(){
+					return $('<div class="thead">').append($(this).contents());
 			}
 	);
 	
-	var $myUl = $('<ul class="table-mobile-tabs clearfix">');
-	$('thead th').each(function()
-	{
-		$myUl.append('<li>'+$(this).text()+'</li>');
-	});
-	$myUl.find("li:first-child").remove(); 
-	$('.table').before($myUl);	
-
-	// replaces thead with <div class="table-head">
-	$('thead').replaceWith(
+	$('tbody').replaceWith(
 			function(){
-					return $('<div class="table-head">').append($(this).contents());
+					return $('<div class="tbody">').append($(this).contents());
 			}
-	);    
+	);
+	
 	// replaces tr with <div class="row">
 	$('tr').replaceWith(
 			function(){
@@ -97,9 +118,26 @@ if ($('table').length > 0)
 					return $('<div class="column">').append($(this).contents());
 			}
 	);
+	
+	// replaces th with <div class="column">
+	$('td').replaceWith(
+			function(){
+					return $('<div class="column">').append($(this).contents());
+			}
+	);
 
 	// Odd and Even Classes
 	$('.row:nth-child(even)').addClass('odd_rows');
+	
+}else if($.browser.version == 7)
+{
+	//IE7 Table Layout css.
+	$('table').addClass('table');
+	$('thead').addClass('thead');
+	$('tbody').addClass('tbody');
+	$('tr').addClass('row');
+	$('tr:nth-child(even)').addClass('odd_rows');
+	$('th, td').addClass('column');
 }
 	
 /*********************************************************/
