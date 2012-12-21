ec848f367df8473ec6505fe23a5cc990755e966b
Pierre Paquet <ppaquet@nurun.com>
2012-12-21 14:26:45 -0800

    Update the page builder module

--- mbuilder/routes/build.js
+++ mbuilder/routes/build.js
@@ -1,107 +1,107 @@
-fs = require('fs');
+fs = require ( 'fs' );
 
-exports.generate = function(req, res) {
-    console.log("yo")
-    var terminal = require('child_process').spawn('bash');
-    var config = require('../config.js');
+exports.generate = function ( req, res ) {
+  console.log ( "yo" )
+  var terminal = require ( 'child_process' ).spawn ( 'bash' );
+  var config = require ( '../config.js' );
 
-    var t = String(req.query.module).replace(/.html(.eco|.hb)/g, '') + "_" + Math.floor(Date.now() / 1000);
-    var p = '../src/html/generated/' + t + ".html.eco";
-    var d = String(fs.readFileSync('module_template.html.eco'));
-    d = d.replace(/{{{{t}}}}/g, t);
-    d = d.replace(/{{{{d}}}}/g, "This module page was generated from the module builder");
-    d = d.replace(/{{{{b}}}}/g, "<%-@partial('modules/" + req.query.module + "', {this:this, data:@data('" + req.query.data + "')})%>");
+  var t = String ( req.query.module ).replace ( /.html(.eco|.hb)/g, '' ) + "_" + Math.floor ( Date.now ( ) / 1000 );
+  var p = '../src/html/generated/' + t + ".html.eco";
+  var d = String ( fs.readFileSync ( 'module_template.html.eco' ) );
+  d = d.replace ( /{{{{t}}}}/g, t );
+  d = d.replace ( /{{{{d}}}}/g, "This module page was generated from the module builder" );
+  d = d.replace ( /{{{{b}}}}/g, "<%-@partial('modules/" + req.query.module + "', {this:this, data:@data('" + req.query.data + "')})%>" );
 
-    fs.writeFile(p, d, 'utf8', function(err) {
-        if (err) {
-            throw err;
-        }
+  fs.writeFile ( p, d, 'utf8', function ( err ) {
+    if ( err ) {
+      throw err;
+    }
 
-        console.log('Sending stdin to terminal');
-        terminal.stdin.write('cd ../src;  grunt mbuilder');
-        terminal.stdin.end();
-    })
+    console.log ( 'Sending stdin to terminal' );
+    terminal.stdin.write ( 'cd ../src;  grunt mbuilder' );
+    terminal.stdin.end ( );
+  } )
 
-    terminal.stdout.on('data', function(data) {
-        console.log('stdout: ' + data);
-    });
+  terminal.stdout.on ( 'data', function ( data ) {
+    console.log ( 'stdout: ' + data );
+  } );
 
-    terminal.on('exit', function(code) {
-        console.log('child process exited with code ' + code);
-        if (code == 0) {
-            res.send(config.localbase + t + '.html');
-        } else {
-            res.send("false");
-        }
+  terminal.on ( 'exit', function ( code ) {
+    console.log ( 'child process exited with code ' + code );
+    if ( code == 0 ) {
+      res.send ( config.localbase + t + '.html' );
+    } else {
+      res.send ( "false" );
+    }
 
-    });
+  } );
 
 }
 
-exports.generatePage = function(req, res) {
+exports.generatePage = function ( req, res ) {
 
-    var terminal = require('child_process').spawn('bash');
-    var config = require('../config.js');
-    var moduleList = "";
+  var terminal = require ( 'child_process' ).spawn ( 'bash' );
+  var config = require ( '../config.js' );
+  var moduleList = "";
 
-    /* PATCH : test if the first element contains more than 1 character
-     *         Correction for a bug when there is only one element, it
-     *         is seen as a STRING.
-     */
-    if (req.body['module'][0].length <= 1) {
-        moduleLength = 1;
+  /* PATCH : test if the first element contains more than 1 character
+   *         Correction for a bug when there is only one element, it
+   *         is seen as a STRING.
+   */
+  if ( req.body['module'] [0].length <= 1 ) {
+    moduleLength = 1;
 
+  } else {
+    moduleLength = req.body ['module'].length;
+  }
+
+  console.log ( 'nb of element(s) : ' + moduleLength );
+
+  for ( var i = 0; i < moduleLength; i++ ) {
+
+    var moduleFileName, moduleDataFileName;
+
+    if ( moduleLength == 1 ) {
+      moduleFileName = req.body ['module'] || null;
+      moduleDataFileName = req.body ['moduleData'] || null;
     } else {
-        moduleLength = req.body['module'].length;
+      moduleFileName = req.body['module'] [i] || null;
+      moduleDataFileName = req.body['moduleData'] [i] || null;
     }
 
-    console.log('nb of element(s) : ' + moduleLength);
+    moduleList = moduleList + "<%-@partial('modules/" + moduleFileName + "',{this:this, data:@data('data/" + moduleDataFileName + "')})%>\n";
+    console.log(moduleList);
+  };
 
-    for (var i = 0; i < moduleLength; i++) {
+  var t = "page" + "_" + Math.floor ( Date.now ( ) / 1000 );
+  var p = '../src/html/generated/' + t + ".html.eco";
+  var d = String ( fs.readFileSync ( 'pages_template.html.eco' ) );
+  // d = d.replace(/{{{{t}}}}/g, t);
+  // d = d.replace(/{{{{d}}}}/g, "This page page was generated from the module builder");
+  d = d.replace ( /{{{{b}}}}/g, moduleList );
 
-        var moduleFileName, moduleDataFileName;
+  fs.writeFile ( p, d, 'utf8', function ( err ) {
+    if ( err ) {
+      throw err;
+    }
 
-        if (moduleLength == 1) {
-            moduleFileName = req.body['module'] || null;
-            moduleDataFileName = req.body['moduleData'] || null;
-        } else {
-            moduleFileName = req.body['module'][i] || null;
-            moduleDataFileName = req.body['moduleData'][i] || null;
-        }
+    console.log ( 'Sending stdin to terminal' );
+    terminal.stdin.write ( 'cd ../src;  grunt mbuilder' );
+    terminal.stdin.end ( );
+  } )
 
-        moduleList = moduleList + "<%-@partial('modules/" + moduleFileName + "',{this:this, data:@data('../data/" + moduleDataFileName + "')})%>\n";
+  terminal.stdout.on ( 'data', function ( data ) {
+    console.log ( 'stdout: ' + data );
+  } );
 
-    };
+  terminal.on ( 'exit', function ( code ) {
+    console.log ( 'child process exited with code ' + code );
+    if ( code == 0 ) {
+      res.send ( config.localbase + t + '.html' );
+    } else {
+      res.send ( "false" );
+    }
 
-    var t = "page" + "_" + Math.floor(Date.now() / 1000);
-    var p = '../src/html/generated/' + t + ".html.eco";
-    var d = String(fs.readFileSync('pages_template.html.eco'));
-   // d = d.replace(/{{{{t}}}}/g, t);
-   // d = d.replace(/{{{{d}}}}/g, "This page page was generated from the module builder");
-    d = d.replace(/{{{{b}}}}/g, moduleList);
-
-    fs.writeFile(p, d, 'utf8', function(err) {
-        if (err) {
-            throw err;
-        }
-
-        console.log('Sending stdin to terminal');
-        terminal.stdin.write('cd ../src;  grunt mbuilder');
-        terminal.stdin.end();
-    })
-
-    terminal.stdout.on('data', function(data) {
-        console.log('stdout: ' + data);
-    });
-
-    terminal.on('exit', function(code) {
-        console.log('child process exited with code ' + code);
-        if (code == 0) {
-            res.send(config.localbase + t + '.html');
-        } else {
-            res.send("false");
-        }
-
-    });
+  } );
 
 }
