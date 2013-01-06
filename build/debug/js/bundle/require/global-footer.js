62c1dbc3195da08617910e7c4ff794728e8426c9
Christopher Mischler <christopher@odopod.com>
2013-01-06 02:31:08 -0800

    Cleaned up old images. Lots of spritesheet updates. Added flags spritesheet. Lots of global nav & footer js updates. Moved search functions into (new file) form-actions.js; Updated _retina.scss (changed to 1.3dpr to serve retina images to Nexus 7)

--- build/debug/js/bundle/require/global-footer.js
+++ build/debug/js/bundle/require/global-footer.js
@@ -1,110 +1,62 @@
-// ------------ Sony Global Nav ------------
-// Module: Global Nav
-// Version: 1.0
-// Modified: 2012-12-17 by Christopher Mischler
+// ------------ GlobalFooter ------------
+// Module: GlobalFooter
+// Version: 0.1
+// Modified: 01/05/2013 by Christopher J Mischler
 // Dependencies: jQuery 1.7+, Modernizr
-// Optional: jQuery throttle-debounce (only used on window resize)
+// 
 // -------------------------------------------------------------------------
-
+ 
 
 (function($, Modernizr, window, undefined) {
+  
+  'use strict';
 
-  $(window).resize(function() {
-    console.log("width: " + $(window).width());
-  });
-
-  var GlobalFooter = function( $container ) {
-    var t = this;
-    t.searchMenu = {};
-    t.$container = $container;
-    t.$activePrimaryNavBtns = t.$container.find('.nav-dropdown-toggle');
-    t.$currentOpenNavBtn = false;
-
-    // we should make a bunch of this stuff global.
-    transEndEventNames = {
-        'WebkitTransition' : 'webkitTransitionEnd',
-        'MozTransition'    : 'transitionend',
-        'OTransition'      : 'oTransitionEnd',
-        'msTransition'     : 'MSTransitionEnd',
-        'transition'       : 'transitionend'
-    };
-
-    // Get the right prefixed names e.g. WebkitTransitionDuration
-    t.tapOrClick = t.hasTouch ? 'touchstart' : 'click';
-    t.transformName = Modernizr.prefixed('transform'); // css version
-    t.transitionName = Modernizr.prefixed('transition');
-    t.transitionProperty = Modernizr.prefixed('transitionProperty');
-    t.transitionDuration = Modernizr.prefixed('transitionDuration');
-    t.transitionEasing = Modernizr.prefixed('transitionTimingFunction');
-    t.transitionEnd = transEndEventNames[t.transitionName];
+  // Start module
+  var GlobalFooter = function(element, options){
+    var self = this;
+    $.extend(self, {}, $.fn.globalFooter.defaults, options, $.fn.globalFooter.settings);
+  }
 
 
-
-    if ( t.mode == 'mobile' ) {
-      // do something
-    }
-
-    // function() { // this needs a debounce
-
-    if ( $(window).width() <= 767 ) {
-      // t.initPrimaryNavBtns(false);
-    } else {
-      // t.initPrimaryNavBtns(true);
-    }
-    // });
-
-    t.isInitialized = true;
-
-    // this should be moved so it doesn't get inited until the search menu is opened.
-    // t.initSearchMenu();
-  };
-
+  // Sample module method
   GlobalFooter.prototype = {
-
     constructor: GlobalFooter,
 
-    initPrimaryNavBtns : function(isDesktop) {
-      var t = this;
-      // console.log("initPrimaryNavBtns: " + isDesktop);
-
+    method : function( param ) {
+     // Method body
     }
   };
 
-    
 
   // Plugin definition
-  $.fn.globalFooter = function( opts ) {
-    var args = Array.prototype.slice.apply( arguments );
+  $.fn.globalFooter = function( options ) {
+    var args = Array.prototype.slice.call( arguments, 1 );
     return this.each(function() {
-      var $this = $(this),
-        globalFooter = $this.data('globalFooter');
-
+      var self = $(this),
+        globalFooter = self.data('globalFooter');
+  
       // If we don't have a stored globalFooter, make a new one and save it
       if ( !globalFooter ) {
-        globalFooter = new GlobalFooter( $this );
-        $this.data( 'globalFooter', globalFooter );
+          globalFooter = new GlobalFooter( self, options );
+          self.data( 'globalFooter', globalFooter );
       }
-
-      if ( typeof opts === 'string' ) {
-        globalFooter[ opts ].apply( globalFooter, args.slice(1) );
+  
+      if ( typeof options === 'string' ) {
+        globalFooter[ options ].apply( globalFooter, args );
       }
     });
   };
 
 
-  // Overrideable options
-  $.fn.globalFooter.options = {
+  // Defaults options for your module
+  $.fn.globalFooter.defaults = { 
+    sampleOption: 0
   };
 
-  // Not overrideable
-  $.fn.globalFooter.settings = {
+  // Non override-able settings
+  $.fn.globalFooter.settings = { 
+    isTouch: !!( 'ontouchstart' in window ),
     isInitialized: false
   };
 
-}(jQuery, Modernizr, window));
-
-
-
-$(".footer-wrapper").globalFooter();
-
-
+ })(jQuery, Modernizr, window, undefined);
