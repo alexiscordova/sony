9be464883a1e46aba918cd0d3d47257fe3081d45
Pierre Paquet <ppaquet@nurun.com>
2013-01-16 16:47:24 -0800

    Commit work before merging master into module-builder. - publishing Jade template work but got error from dependencies.

--- mbuilder/public/js/pages-builder.js
+++ mbuilder/public/js/pages-builder.js
@@ -1,97 +1,266 @@
-var mb = {};
+var moduleGroupContainer = $('<div class="main-module-container">'), moduleGroup = $(
+/* @formatter:off */
+    '<div class="clearfix form-inline row modContainer">' + 
+    ' <div class="control-group span4">' + 
+    '   <label>Module</label>' + 
+    '   <select class="module_select"  name="module">' + 
+    '     <option class="hold">Select a module</option>' +     
+    '   </select>' + 
+    ' </div>' + 
+    ' <div class="data_control control-group span4">' + 
+    '   <label>Data</label>' + 
+    '   <select class="data_select" name="moduleData"></select>' + 
+    ' </div>' + 
+    '</div>'
+/* @formatter:on */
+), submodContainer = $(
+/* @formatter:off */
+    '<div class="clearfix form-inline submodContainer">' +
+    '<h4>Submodules</h4>' +
+    '   <div class="span2 btn-addSubModule">' +
+    '       <button class="btn btn-small addSubModule" >' +
+    '           Define a Sub-module '+
+    '       </button>' +
+    '   </div>' +
+    '</div>'
+/* @formatter:on */
+);
 
-mb.isSuperModule = false;
+var mainModuleContainer, subModuleContainer;
+
+var mb = {}, selectedElem;
+
 mb.moduleName = "";
-mb.modulePath = ""
+mb.modulePath = "";
 mb.moduleData = {};
+
 mb.subModules = [];
 mb.modulePaths = [];
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   /** Add module block **/
-  $("#add-module").click(function (e) {
-
-    var _this = this, modulesContainer = $("#main-forms-container"), moduleGroup =
-    /* @formatter:off */
-                            '<div class="clearfix form-inline row">'+
-                            '    <div class="control-group span4">'+
-                            '        <label>Module</label>'+
-                            '        <select class="module_select">'+
-                            '           <option class="hold"></option>'+
-                            '        </select>'+
-                            '   </div>'+
-                    
-                            '    <div class="data_control control-group span4">'+
-                            '        <label>Data</label>'+
-                            '        <select class="data_select"></select>'+
-                            '     </div>'+
-                            '</div>';
-         /* @formatter:on */
-
-    $(modulesContainer).append(moduleGroup).change(function (elem) {
-      onModuleSelect(elem)
-    });
-
-    getModuleName();
-
+  $("#add-module").click(function(e) {
+    createModuleContainer(this);
   });
 
-});
+  createModuleContainer();
 
-function getModuleName (selectedModule) {
+});
+/** Document Ready end here**/
+
+/*************************************
+ *      Call to web services         *
+ *************************************/
+
+function getModuleName() {
   $.ajax({
     url : "/mnames"
-  }).done(function (res) {
-      
+  }).done(function(res) {
+
     mb.modulePaths = JSON.parse(res);
-    
-    $.each(mb.modulePaths, function (i, e) {
-        
-      $('.module_select').append("<option value=" + e + ">" + e.replace(/.html(.eco|.hb)/g, '') + "</option>")
-    })
-    
-    
+
+    $.each(mb.modulePaths, function(i, e) {
+      $(".module_select").append("<option value=" + e + ">" + e.replace(/.html(.eco|.hb|.jade)/g, '') + "</option>");
+    });
+
+    $(".module_select").change(function(elem) {
+      $(".module_select .hold").remove();
+      getDataList(elem);
+    });
+
+    $(".data_select").change(function(elem) {
+      getJsonContent(this, elem);
+    });
+
   });
 }
 
-function onModuleSelect (elem) {
+function getDataList(elem) {
 
-  var moduleContainer = $(elem.target).parents(".form-inline"), 
-  dataControl = $(moduleContainer).find(".data_control"), 
-  dataSelect = $(moduleContainer).find(".data_select");
-  mb.modulePath = elem.target.value.replace (/((module_)|(.html(.eco|.hb)))/g, '');
-  mb.moduleName = mb.modulePath.replace(/.html(.eco|.hb)/g, '');
+  /* @formatter:off */
+  var moduleContainer = $(elem.target).parents(".form-inline")[0], 
+      dataControl = $(moduleContainer).find(".data_control"), 
+      dataSelect = $(moduleContainer).find(".data_select");
+/* @formatter:on */
 
-  $(dataSelect).show();
+  mb.modulePath = elem.target.value.replace(/((module_)|(.html(.eco|.hb|.jade)))/g, '');
+  mb.moduleName = mb.modulePath.replace(/.html(.eco|.hb|.jade)/g, '');
 
   $.ajax({
     url : "/dnames",
     data : {
       mname : mb.moduleName
     }
-  }).done(function (res) {
+  }).done(function(res) {
+
     var dnames = JSON.parse(res);
-
-    if (mb.isSuperModule) {
-
-    }
 
     $(dataControl).toggle(dnames.length > 0);
     $(dataControl).toggleClass('disabled', dnames.length == 0);
     $(dataSelect).html("");
-    
-    $.each (dnames, function (i, e) {
-        $ (dataSelect).append ("<option value=" + mb.moduleName + "/" + e + ">" + e + "</option>")
-        if (e == "default.json") {
-          $ (dataSelect).val (mb.moduleName + "/" + e);
-        }
-      })
-      
-      $ (dataSelect).trigger ("change");
-      
-    var temp;
+
+    $.each(dnames, function(i, e) {
+
+      $(dataSelect).append("<option value=" + mb.moduleName + "/" + e + ">" + e + "</option>")
+      if (e == "default.json") {
+        $(dataSelect).val(mb.moduleName + "/" + e);
+      }
+
+    })
+
+     $(dataSelect).trigger("change");
+
   });
 
 }
+
+function getJsonContent(dataSelect, elem) {
+
+  selectedElem = $(dataSelect);
+
+  $.ajax({
+    url : "/getjson",
+    data : {
+      path : elem.target.value
+    }
+  }).done(function(res) {
+
+    addModule(res);
+
+  });
+
+}
+
+/*************************************
+ *              Utilities            *
+ *************************************/
+
+/* create the superModule Node if it undefined */
+function addSuperModuleNode() {
+  $.extend(mb.moduleData, {
+    "config" : {
+      "maxSubModule" : 0
+    }
+  })
+};
+
+/*************************************
+ *     Dom Manipulation functions    *
+ *************************************/
+
+function createModuleContainer(elem) {
+  modulesContainer = $("#main-forms-container");
+
+  $moduleGroupContainer = moduleGroupContainer.clone(true);
+  $moduleGroup = moduleGroup.clone(true);
+  $submodContainer = submodContainer.clone(true);
+
+  $($submodContainer).find("button.addSubModule").bind("click", function(e) {
+    e.preventDefault();
+    addSubModule(this);
+  });
+
+  $($moduleGroupContainer).append($moduleGroup,$submodContainer);
+  
+  $(modulesContainer).append($moduleGroupContainer);
+
+  getModuleName();
+
+};
+
+function addModule(res) {
+  //parse json
+  mb.moduleData = JSON.parse(res);
+
+  //if it contains submodules
+  mb.subModules = mb.moduleData.submodules
+
+  /* test if node is undefined*/
+  if (!(mb.moduleData.config && mb.moduleData.config.maxSubModule)) {
+    addSuperModuleNode();
+  }
+
+  if (mb.moduleData.config.maxSubModule > 0 ) {
+
+    subModuleContainer = $(selectedElem).parents(".main-module-container").find(".submodContainer");
+
+    $(subModuleContainer).show();
+
+  } else if($(selectedElem).parents(".submodContainer").length === 0) {
+    $(selectedElem).parents(".main-module-container").find(".submodContainer").hide();
+  }
+}
+
+function addSubModule(elem) {
+
+  /* @formatter:off */
+  var subModContainer = $(elem).parent(".btn-addSubModule"),
+      $moduleGroup = moduleGroup.clone(true);
+ /* @formatter:on */
+
+  $(subModContainer).before($moduleGroup);
+  getModuleName();
+};
+
+
+
+
+
+
+
+
+
+ // when you hit build run the generate script
+  $("#build").bind('click', function(e) {
+
+    var myForm = $("#myForm");
+
+   // $("#build").button('loading');
+
+    $.ajax({
+      type : "POST",
+      url : myForm.attr('action'),
+      data : myForm.serialize(),
+      
+      success : function(res) {
+        if (res !== "false" && res !== false) {
+          $("#build").button('reset');
+          if ($('.alert').length > 0) {
+            $('.alert').remove();
+          }
+          $('body').prepend('<div id="build_success" class="alert alert-success">Success! Check it out here: <a target="_blank" href="http://' + res + '">' + res + '</a><button type="button" class="close" data-dismiss="alert">×</button></div>');
+          $('#build_success').alert();
+        } else {
+          if ($('.alert').length > 0) {
+            $('.alert').remove();
+          }
+          $('body').prepend('<div id="build_success" class="alert alert-success">ERROR: Build Failed!<button type="button" class="close" data-dismiss="alert">×</button></div>');
+          $('#build_success').alert();
+        }
+
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+        $('body').prepend('<div id="build_success" class="alert alert-success">ERROR: Check it out here: The AJax call return an error : ' + errorThrown + '<button type="button" class="close" data-dismiss="alert">×</button></div>');
+        $('#build_success').alert();
+      }
+
+    });
+
+  });
+
+  //turn build into a bootstrap button
+  $("#build").button();
+
+
+
+
+
+
+
+
+
+
+
+
 
--- mbuilder/routes/build.js
+++ mbuilder/routes/build.js
@@ -1,67 +1,68 @@
-fs = require ( 'fs' );
+fs = require('fs');
 
-exports.generate = function ( req, res ) {
-  console.log ( "yo" )
-  var terminal = require ( 'child_process' ).spawn ( 'bash' );
-  var config = require ( '../config.js' );
+exports.generate = function (req, res) {
+  console.log("yo")
+  var terminal = require('child_process').spawn('bash');
+  var config = require('../config.js');
 
-  var t = String ( req.query.module ).replace ( /.html(.eco|.hb)/g, '' ) + "_" + Math.floor ( Date.now ( ) / 1000 );
+  var t = String(req.query.module).replace(/.html(.eco|.hb)/g, '') + "_" + Math.floor(Date.now() / 1000);
   var p = '../src/html/generated/' + t + ".html.eco";
-  var d = String ( fs.readFileSync ( 'module_template.html.eco' ) );
-  d = d.replace ( /{{{{t}}}}/g, t );
-  d = d.replace ( /{{{{d}}}}/g, "This module page was generated from the module builder" );
-  d = d.replace ( /{{{{b}}}}/g, "<%-@partial('modules/" + req.query.module + "', {this:this, data:@data('" + req.query.data + "')})%>" );
+  var d = String(fs.readFileSync('module_template.html.eco'));
+  d = d.replace(/{{{{t}}}}/g, t);
+  d = d.replace(/{{{{d}}}}/g, "This module page was generated from the module builder");
+  d = d.replace(/{{{{b}}}}/g, 'e = {"locals":locals, "data":data("' + req.query.data + '")}');
+  d = d.replace(/{{{{c}}}}/g, "!{partial('modules/" + req.query.module + "', e)}");
 
-  fs.writeFile ( p, d, 'utf8', function ( err ) {
-    if ( err ) {
+  fs.writeFile(p, d, 'utf8', function (err) {
+    if (err) {
       throw err;
     }
 
-    console.log ( 'Sending stdin to terminal' );
-    terminal.stdin.write ( 'cd ../src;  grunt mbuilder' );
-    terminal.stdin.end ( );
-  } )
+    console.log('Sending stdin to terminal');
+    terminal.stdin.write('cd ../src;  grunt mbuilder');
+    terminal.stdin.end();
+  })
 
-  terminal.stdout.on ( 'data', function ( data ) {
-    console.log ( 'stdout: ' + data );
-  } );
+  terminal.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
 
-  terminal.on ( 'exit', function ( code ) {
-    console.log ( 'child process exited with code ' + code );
-    if ( code == 0 ) {
-      res.send ( config.localbase + t + '.html' );
+  terminal.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+    if (code == 0) {
+      res.send(config.localbase + t + '.html');
     } else {
-      res.send ( "false" );
+      res.send("false");
     }
 
-  } );
+  });
 
 }
 
-exports.generatePage = function ( req, res ) {
+exports.generatePage = function (req, res) {
 
-  var terminal = require ( 'child_process' ).spawn ( 'bash' );
-  var config = require ( '../config.js' );
+  var terminal = require('child_process').spawn('bash');
+  var config = require('../config.js');
   var moduleList = "";
 
   /* PATCH : test if the first element contains more than 1 character
    *         Correction for a bug when there is only one element, it
    *         is seen as a STRING.
    */
-  if ( req.body['module'] [0].length <= 1 ) {
+  if (req.body['module'] [0].length <= 1) {
     moduleLength = 1;
 
   } else {
     moduleLength = req.body ['module'].length;
   }
 
-  console.log ( 'nb of element(s) : ' + moduleLength );
+  console.log('nb of element(s) : ' + moduleLength);
 
   for ( var i = 0; i < moduleLength; i++ ) {
 
     var moduleFileName, moduleDataFileName;
 
-    if ( moduleLength == 1 ) {
+    if (moduleLength == 1) {
       moduleFileName = req.body ['module'] || null;
       moduleDataFileName = req.body ['moduleData'] || null;
     } else {
@@ -69,39 +70,40 @@ exports.generatePage = function ( req, res ) {
       moduleDataFileName = req.body['moduleData'] [i] || null;
     }
 
-    moduleList = moduleList + "<%-@partial('modules/" + moduleFileName + "',{this:this, data:@data('data/" + moduleDataFileName + "')})%>\n";
+    moduleList = moduleList + 'e = {"locals":locals, "data":data("' + moduleDataFileName + '")}\r    !{partial("modules/' + moduleFileName + '", e)}\r';
     console.log(moduleList);
   };
 
-  var t = "page" + "_" + Math.floor ( Date.now ( ) / 1000 );
-  var p = '../src/html/generated/' + t + ".html.eco";
-  var d = String ( fs.readFileSync ( 'pages_template.html.eco' ) );
+
+  var t = "page" + "_" + Math.floor(Date.now() / 1000);
+  var p = '../src/html/generated/' + t + ".html.jade";
+  var d = String(fs.readFileSync('page-builder-template.jade'));
   // d = d.replace(/{{{{t}}}}/g, t);
   // d = d.replace(/{{{{d}}}}/g, "This page page was generated from the module builder");
-  d = d.replace ( /{{{{b}}}}/g, moduleList );
+  d = d.replace(/{{{{b}}}}/g, moduleList);
 
-  fs.writeFile ( p, d, 'utf8', function ( err ) {
-    if ( err ) {
+  fs.writeFile(p, d, 'utf8', function (err) {
+    if (err) {
       throw err;
     }
 
-    console.log ( 'Sending stdin to terminal' );
-    terminal.stdin.write ( 'cd ../src;  grunt mbuilder' );
-    terminal.stdin.end ( );
-  } )
+    console.log('Sending stdin to terminal');
+    terminal.stdin.write('cd ../src;  grunt mbuilder');
+    terminal.stdin.end();
+  })
 
-  terminal.stdout.on ( 'data', function ( data ) {
-    console.log ( 'stdout: ' + data );
-  } );
+  terminal.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
 
-  terminal.on ( 'exit', function ( code ) {
-    console.log ( 'child process exited with code ' + code );
-    if ( code == 0 ) {
-      res.send ( config.localbase + t + '.html' );
+  terminal.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+    if (code == 0) {
+      res.send(config.localbase + t + '.html');
     } else {
-      res.send ( "false" );
+      res.send("false");
     }
 
-  } );
+  });
 
 }
