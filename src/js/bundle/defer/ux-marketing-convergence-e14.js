34d98357e16b1d3159bf6b1019a9f18685813a3d
George Pantazis <george.pantazis@odopod-Mac-Pro-Station-343.local>
2013-01-18 18:16:44 -0800

    E14: Fix indentation of JS file

--- src/js/bundle/defer/ux-marketing-convergence-e14.js
+++ src/js/bundle/defer/ux-marketing-convergence-e14.js
@@ -8,108 +8,108 @@
 
 (function($) {
 
-    'use strict';
+  'use strict';
 
-    var MarketingConvergenceModule = function($element, options){
+  var MarketingConvergenceModule = function($element, options){
+
+    var self = this;
+
+    $.extend(self, {}, $.fn.marketingConvergenceModule.defaults, options);
+
+    self.$el = $element;
+
+    self.init();
+  };
+
+  MarketingConvergenceModule.prototype = {
+
+    'constructor': MarketingConvergenceModule,
+
+    'init': function() {
 
       var self = this;
 
-      $.extend(self, {}, $.fn.marketingConvergenceModule.defaults, options);
+      self.buildPartnerCarousel();
+    },
 
-      self.$el = $element;
+    'buildPartnerCarousel': function() {
 
-      self.init();
-    };
+      var self = this;
 
-    MarketingConvergenceModule.prototype = {
+      self.currentPartnerProduct = -1;
+      self.$partnerCarousel = self.$el.find('.partner-products');
+      self.$partnerCarouselSlides = self.$partnerCarousel.find('li');
 
-      'constructor': MarketingConvergenceModule,
+      self.$partnerCarouselSlides.detach();
 
-      'init': function() {
+      self.gotoNextPartnerProduct();
+      self.setPartnerCarouselInterval();
+    },
 
-        var self = this;
+    'setPartnerCarouselInterval': function() {
 
-        self.buildPartnerCarousel();
-      },
+      var self = this;
 
-      'buildPartnerCarousel': function() {
-
-        var self = this;
-
-        self.currentPartnerProduct = -1;
-        self.$partnerCarousel = self.$el.find('.partner-products');
-        self.$partnerCarouselSlides = self.$partnerCarousel.find('li');
-
-        self.$partnerCarouselSlides.detach();
-
-        self.gotoNextPartnerProduct();
-        self.setPartnerCarouselInterval();
-      },
-
-      'setPartnerCarouselInterval': function() {
-
-        var self = this;
-
-        if ( self.partnerCarouselInterval ) {
-          clearInterval(self.partnerCarouselInterval);
-        }
-
-        self.partnerCarouselInterval = setInterval(function(){
-          self.gotoNextPartnerProduct();
-        }, self.rotationSpeed);
-      },
-
-      'gotoNextPartnerProduct': function() {
-
-        var self = this,
-            $newSlide;
-
-        if ( self.currentPartnerProduct === self.$partnerCarouselSlides.length - 1 ) {
-          self.currentPartnerProduct = 0;
-        } else {
-          self.currentPartnerProduct++;
-        }
-
-        self.$partnerCarousel.children().each(function(){
-          $(this).fadeOut(self.transitionTime, function(){
-            $(this).remove();
-          });
-        });
-
-        $newSlide = self.$partnerCarouselSlides.eq(self.currentPartnerProduct).clone();
-
-        $newSlide.appendTo(self.$partnerCarousel);
-        $newSlide.fadeOut(0).fadeIn(self.transitionTime);
-
-        window.iQ.update();
+      if ( self.partnerCarouselInterval ) {
+        clearInterval(self.partnerCarouselInterval);
       }
-    };
 
-    $.fn.marketingConvergenceModule = function( options ) {
-      var args = Array.prototype.slice.call( arguments, 1 );
-      return this.each(function() {
-        var self = $(this),
-          marketingConvergenceModule = self.data('marketingConvergenceModule');
+      self.partnerCarouselInterval = setInterval(function(){
+        self.gotoNextPartnerProduct();
+      }, self.rotationSpeed);
+    },
 
-        // If we don't have a stored marketingConvergenceModule, make a new one and save it
-        if ( !marketingConvergenceModule ) {
-            marketingConvergenceModule = new MarketingConvergenceModule( self, options );
-            self.data( 'marketingConvergenceModule', marketingConvergenceModule );
-        }
+    'gotoNextPartnerProduct': function() {
 
-        if ( typeof options === 'string' ) {
-          marketingConvergenceModule[ options ].apply( marketingConvergenceModule, args );
-        }
+      var self = this,
+          $newSlide;
+
+      if ( self.currentPartnerProduct === self.$partnerCarouselSlides.length - 1 ) {
+        self.currentPartnerProduct = 0;
+      } else {
+        self.currentPartnerProduct++;
+      }
+
+      self.$partnerCarousel.children().each(function(){
+        $(this).fadeOut(self.transitionTime, function(){
+          $(this).remove();
+        });
       });
-    };
 
-    $.fn.marketingConvergenceModule.defaults = {
-      'rotationSpeed': 5000,
-      'transitionTime': 1000
-    };
+      $newSlide = self.$partnerCarouselSlides.eq(self.currentPartnerProduct).clone();
 
-    $(function(){
-     $('.uxmc-container').marketingConvergenceModule();
+      $newSlide.appendTo(self.$partnerCarousel);
+      $newSlide.fadeOut(0).fadeIn(self.transitionTime);
+
+      window.iQ.update();
+    }
+  };
+
+  $.fn.marketingConvergenceModule = function( options ) {
+    var args = Array.prototype.slice.call( arguments, 1 );
+    return this.each(function() {
+      var self = $(this),
+        marketingConvergenceModule = self.data('marketingConvergenceModule');
+
+      // If we don't have a stored marketingConvergenceModule, make a new one and save it
+      if ( !marketingConvergenceModule ) {
+          marketingConvergenceModule = new MarketingConvergenceModule( self, options );
+          self.data( 'marketingConvergenceModule', marketingConvergenceModule );
+      }
+
+      if ( typeof options === 'string' ) {
+        marketingConvergenceModule[ options ].apply( marketingConvergenceModule, args );
+      }
     });
+  };
 
- })(jQuery);
+  $.fn.marketingConvergenceModule.defaults = {
+    'rotationSpeed': 5000,
+    'transitionTime': 1000
+  };
+
+  $(function(){
+   $('.uxmc-container').marketingConvergenceModule();
+  });
+
+})(jQuery);
