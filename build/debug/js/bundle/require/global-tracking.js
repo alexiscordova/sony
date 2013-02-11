8e8b56df25b72738c572954993c5393046ed075a
Pierre Paquet <ppaquet@nurun.com>
2013-02-11 15:12:12 -0800

    - remove semicolon in brightcove-builder and global-tracking js files - pages in the generated folder are now build in deploy

--- build/debug/js/bundle/require/global-tracking.js
+++ build/debug/js/bundle/require/global-tracking.js
@@ -1,6 +1,5 @@
 /*global jQuery, brightcove*/
 
-
 // Global tracking
 // ---------------------------------------------------------------
 //
@@ -10,15 +9,14 @@
 // * **Author:** Pierre Paquet
 // * **Dependencies:**  jQuery 1.7+
 
-;(function( $ ) {
+(function($, undefined) {
   
   'use strict';
 
+  var GlobalTracking = function($element, options) {
 
-  var GlobalTracking = function( $element, options ) {
-
-    var self =        this;  
-    self.$el =        $element;
+    var self = this;
+    self.$el = $element;
 
     self.init( );
 
@@ -29,57 +27,54 @@
     'constructor' : GlobalTracking,
 
     'init' : function() {
-      
+
       var self = this;
-      self.clickEvent();
-      
+      self.clickEvent( );
+
     },
     'clickEvent' : function() {
-      
+
       var self = this;
-      
+
       // We track all click inside module
-      // **TODO:** This temporary, should be modify to reflect tracking strategy. 
-      self.$el.on( 'click.tracking', function( e ) {
-        console.log('click');
-        var $elem         = $(e.target).parents( '*[data-module-id]'),
-            elemData      = $elem.data('trackingLog') || [],
-            trackingData  = elemData;
-            
-        $elem.type            = e.type;
-        $elem.elemID          = self.$el.data('moduleId'); 
-        $elem.elemName        =    self.$el.data('moduleName');  
+      // **TODO:** This temporary, should be modify to reflect tracking strategy.
+      self.$el.on( 'click.tracking', function(e) {
+        console.log( 'click' );
+        var $elem = $( e.target ).parents( '*[data-module-id]' ), elemData = $elem.data( 'trackingLog' ) || [], trackingData = elemData;
+
+        $elem.type = e.type;
+        $elem.elemID = self.$el.data( 'moduleId' );
+        $elem.elemName = self.$el.data( 'moduleName' );
         $elem.$actionInitElem = e.target;
-        
+
         // Create an array of elements we are tracking
         var trackingTable = [$elem.type, $elem.$actionInitElem, $elem.elemID, $elem.elemName];
-        
-        elemData.push(trackingTable);
-        
-        $elem.data('trackingLog',trackingData);
 
-      });
+        elemData.push( trackingTable );
+
+        $elem.data( 'trackingLog', trackingData );
+
+      } );
     }
 
   };
-  
-  
-  jQuery.fn.globalTracking = function( options ) {
+
+  jQuery.fn.globalTracking = function(options) {
 
     var args = Array.prototype.slice.call( arguments, 1 );
 
-    return this.each( function( ) {
+    return this.each( function() {
       var self = $( this ), globalTracking = self.data( 'globalTracking' );
 
       if ( !globalTracking ) {
-        globalTracking = new GlobalTracking( self, options );
+        globalTracking = new GlobalTracking(self, options);
         self.data( 'globalTracking', globalTracking );
       }
 
       if ( typeof options === 'string' ) {
         globalTracking[options].apply( globalTracking, args );
       }
-    });
+    } );
 
   };
 
