6de85b965105ace8dc821c7856c6218f15c890a8
Glen Cheney <glen@odopod.com>
2013-02-28 10:12:26 -0800

    Fixes mutli spec scroller for browsers w/o transforms. SGFE-305

--- src/js/bundle/defer/spec-multi-z2.js
+++ src/js/bundle/defer/spec-multi-z2.js
@@ -456,6 +456,12 @@
         .scrollspy('refresh')
         .scrollspy('process');
 
+      // Without transforms, iScroll uses absolute positioning and the container
+      // gets a width/height of 0 with overflow:hidden
+      if ( !Modernizr.csstransforms ) {
+        self._setScrollerWrapperDimensions();
+      }
+
       // Set timeout here because we were getting the wrong height for the
       // spec products after a big resize
       setTimeout(function() {
@@ -566,6 +572,18 @@
         top: top,
         bottom: bottom
       };
+    },
+
+    _setScrollerWrapperDimensions : function() {
+      var self = this,
+          containerWidth = self.$specItemsGrid.width(),
+          containerHeight = self.$specItemsGrid.height(),
+          labelsWidth = self.$detailLabelsWrap.width(),
+          scrollerWidth = containerWidth - labelsWidth - 2; // not sure why it doesn't fit
+
+      self.$specItemsWrap
+        .width( scrollerWidth )
+        .height( containerHeight );
     }
 
   };
