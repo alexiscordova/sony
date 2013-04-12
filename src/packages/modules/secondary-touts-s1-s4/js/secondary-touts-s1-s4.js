515b016aef952f5e9105a1fde94342c0a29f35ee
George Pantazis <george@odopod.com>
2013-04-12 15:28:24 -0700

    Updates to secondary tout system

--- src/packages/modules/secondary-touts-s1-s4/js/secondary-touts-s1-s4.js
+++ src/packages/modules/secondary-touts-s1-s4/js/secondary-touts-s1-s4.js
@@ -13,7 +13,9 @@ define(function(require){
   'use strict';
 
   var $ = require('jquery'),
-      iQ = require('iQ');
+      iQ = require('iQ'),
+      enquire = require('enquire'),
+      Settings = require('require/sony-global-settings');
 
   var module = {
     'init': function() {
@@ -29,6 +31,7 @@ define(function(require){
 
     self.$el = $(element);
     self.$images = self.$el.find('.st-image');
+    self.$items = self.$el.find('.st-item');
 
     self.init();
 
@@ -37,6 +40,19 @@ define(function(require){
     });
 
     self.$images.addClass('iq-img');
+
+    if ( !Settings.$html.hasClass('lt-ie10') ){
+
+      enquire.register("(min-width: 780px)", function() {
+        self.renderDesktop();
+      });
+      enquire.register("(max-width: 779px)", function() {
+        self.renderEvenColumns(6);
+      });
+
+    } else {
+      self.renderDesktop();
+    }
   };
 
   SecondaryTouts.prototype = {
@@ -45,8 +61,46 @@ define(function(require){
 
     init: function() {
       var self = this;
-    }
 
+      self.$items.each(function(){
+
+        var matchedClasses = this.className.split(' ').filter(function(a){
+          return a.search('span') === 0;
+        });
+
+        $(this).data('originalWidth', matchedClasses[0]);
+      });
+    },
+
+    // Create or restore the default slide layout.
+
+    renderDesktop: function(which) {
+
+      var self = this;
+
+      self.$items.each(function(){
+
+        var $this = $(this);
+
+        $this.removeClass('span12 span8 span6 span4').addClass($this.data('originalWidth'));
+      });
+    },
+
+    renderEvenColumns: function(colPerItem) {
+
+      var self = this;
+
+      self.$items.each(function(){
+
+        var $this = $(this);
+
+        if ( $this.data('originalWidth') === 'span12' ) {
+          return;
+        }
+
+        $this.removeClass('span12 span8 span6 span4').addClass('span' + colPerItem);
+      });
+    }
   };
 
   return module;
