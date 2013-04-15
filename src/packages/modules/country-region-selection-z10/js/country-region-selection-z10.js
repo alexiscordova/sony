252649623675fc07cc9ad9ca8a71a449ad531ed2
Chris Pickett <cpickett@nerdery.com>
2013-04-15 16:01:56 -0500

    Adds relevant comments to country-region-selection-z10.js

--- src/packages/modules/country-region-selection-z10/js/country-region-selection-z10.js
+++ src/packages/modules/country-region-selection-z10/js/country-region-selection-z10.js
@@ -1,7 +1,7 @@
 /*global define, Modernizr, log*/
 
 // -------- Sony Country Region Selection -------
-// Module: Country Region Selection
+// Module: Country / Region Selection
 // Version: 1.0
 // Author: Chris Pickett
 // Date: 04/10/13
@@ -21,6 +21,8 @@ define(function(require){
   var COLUMNS_IN_GRID = 12;
   var MIN_COUNTRIES_PER_COLUMN = 5;
 
+
+  // View Controller for country / region selection
   var CountryRegionSelection = {
     init: function() {
       this.stickyHeader = this.stickyHeader = StickyHeader.init();
@@ -30,6 +32,7 @@ define(function(require){
       return this.bind();
     },
 
+    // Binds enquire handlers to setup page for mobile, tablet and desktop.
     bind : function() {
       window.enquire.register('(max-width: 767px)', {
         match : $.proxy(this.toMobile, this)
@@ -46,6 +49,7 @@ define(function(require){
       return this;
     },
 
+    // Gets a FluidList object for each country list.
     getFluidLists : function() {
       var lists = [];
 
@@ -56,6 +60,7 @@ define(function(require){
       return lists;
     },
 
+    // Calls reset on each FluidList
     resetFluidLists : function() {
       var i = 0,
           numLists = this.fluidLists.length;
@@ -65,6 +70,7 @@ define(function(require){
       }
     },
 
+    // Calls update on each FluidList
     updateFluidLists : function(tag) {
       var i = 0,
           numLists = this.fluidLists.length;
@@ -74,22 +80,27 @@ define(function(require){
       }
     },
 
+    // Called when media query matches mobile.
     toMobile : function() {
       this.stickyHeader.enable();
       this.resetFluidLists();
     },
 
+    // Called when media query matches tablet.
     toTablet : function() {
       this.stickyHeader.disable();
       this.updateFluidLists('tablet');
     },
 
+    // Called when media query matches desktop.
     toDesktop : function() {
       this.stickyHeader.disable();
       this.updateFluidLists('desktop');
     }
   };
 
+
+  // Handles continent name sticky header for mobile.
   var StickyHeader = {
     init: function() {
       this.$fixedHeader = $('.continent-sticky-header').hide();
@@ -106,6 +117,7 @@ define(function(require){
       return this._getHeaderOffsets();
     },
 
+    // Gets the offsets of each of the headers.
     _getHeaderOffsets: function() {
       var offsets = [];
 
@@ -120,6 +132,7 @@ define(function(require){
       return this;
     },
 
+    // Enables iScroll and makes sure sticky header is correctly aligned.
     enable: function() {
       this.scroll = new IScroll('scrollable', {
         onScrollMove: this.scrollHandler.bind(this),
@@ -132,6 +145,7 @@ define(function(require){
       return this;
     },
 
+    // Hides the fixed header and destroys the iscroll.
     disable: function() {
       this.$fixedHeader.hide();
 
@@ -142,15 +156,18 @@ define(function(require){
       return this;
     },
 
+    // Called by iScroll when a scroll event happens.
     scrollHandler: function() {
       var offsetTarget = Math.abs(this.scroll.y); // iScroll.y is negative
       this.updateFixedHeader(offsetTarget);
     },
 
+    // Updates the fixed header with the title of the closest header.
     updateFixedHeader: function(targetOffset) {
       var $header,
           headerIndex = this._indexOfClosestHeader(targetOffset);
 
+      // Only update the fixed header if the new header is different.
       if (headerIndex != this.currentHeader) {
         this.currentHeader = headerIndex;
         $header = $(this.$headers[headerIndex]);
@@ -162,12 +179,15 @@ define(function(require){
         }
       }
 
+      // Hide the fixed header if there is no header to display.
       if (headerIndex === -1 && this.headerIsVisible) {
           this.$fixedHeader.stop().hide();
           this.headerIsVisible = false;
       }
     },
 
+    // Gets the index of the closest header to the target offset.
+    // Closest being the header that is not in view (a negative offset) and nearest to the target offset.
     _indexOfClosestHeader: function (targetOffset) {
       var headerIndex = -1,
           i = 0,
@@ -187,6 +207,11 @@ define(function(require){
     }
   };
 
+
+  // FluidList
+  // Takes a html list and will make it fit into the given containers
+
+  // FluidList constructor
   function FluidList(numGridColumns, $list, minItemsPerContainer) {
     return this.init(numGridColumns, $list, minItemsPerContainer);
   }
@@ -206,6 +231,7 @@ define(function(require){
       return this;
     },
 
+    // Gets an array of the items in the list.
     _getItemsInList: function() {
       var items = [];
 
@@ -217,11 +243,13 @@ define(function(require){
       return items;
     },
 
+    // Gets the containers from the markup. Uses the rel attribute.
     _getContainers: function() {
       var listContainerRel = this.$list.attr(this.CONTAINERS_REL_ATTR);
       return $("[rel=" + listContainerRel + "]");
     },
 
+    // Maps the containers to their tags.
     _getContainersMap: function() {
       var tagAttr = this.MEDIA_TAG_ATTR,
           $list = this.$list,
@@ -250,10 +278,12 @@ define(function(require){
       return containersMap;
     },
 
+    // Returns the correct set of containers for the given tag.
     _getContainersForTag: function(tag) {
         return this.containersMap[tag];
     },
 
+    // Reflows the items to fit into the containers for the given tag.
     update: function(tag) {
       var containers = this._getContainersForTag(tag),
           containerWidth = this.numGridColumns / containers.length,
@@ -285,10 +315,12 @@ define(function(require){
       }
     },
 
+    // Removes the items from the containers and puts them back into the initial list and makes sure that span classes are removed from the containers.
     reset: function() {
       return this._resetItems()._resetContainers();
     },
 
+    // Removes the items from the containers and puts them back into the initial list.
     _resetItems: function() {
       var i = 0,
           numItems = this.items.length;
@@ -300,6 +332,7 @@ define(function(require){
       return this;
     },
 
+    // Removes all classes from the containers
     _resetContainers: function() {
       this.$containers.removeClass();
       return this;
