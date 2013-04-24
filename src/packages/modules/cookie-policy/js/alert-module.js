09bb066226ae7adff85679fbe58d0df1a6b69b16
cujojp <info@cujo.jp>
2013-04-23 18:25:35 -0700

    working on the alert (cookie) module

--- src/packages/modules/cookie-policy/js/alert-module.js
+++ src/packages/modules/cookie-policy/js/alert-module.js
@@ -1,4 +1,5 @@
 /*global log*/
+/*jshint debug:true */
 
 // ------------ Sony Alert ------------
 // Module: Alert Module
@@ -8,31 +9,92 @@
 // --------------------------------------
 
 define(function(require){
-  var defaults;
+  var defaults,
+      AlertModule,
+      module;
 
+  // Public methods
+  // --------------
+  module = {
+    'init': function() {
+      new AlertModule();
+    }
+  };
+  
   defaults = {
-
+    allowsCookies : true,
+    alertModule : '#cookie-alert'
   };
 
-  alertModule = function( options ) {
-
-    var AlertModule,
-    $win = $(window),
-    helpers;
+  AlertModule = function( options ) {
+    $win = $(window);
 
     // Map optional configs if they exist  
     options = this.options = options ? $.extend({}, defaults, options) : defaults;
 
+    this.allowsCookies = options.allowsCookies;
+    this.$alertModule = $( options.alertModule );
+
+    // init :)
     this.init();
   };
 
   // AlertModule Methods
   AlertModule.prototype = {
 
+    // Check our faux setting if user allows cookies or not
+    _checkCookies: function() {
+
+      // if the user allows cookies we can display the error
+      if (this.allowsCookies) { 
+        this._displayAlert();
+      }
+
+    },
+      
+    // show ther alert 
+    _displayAlert: function() {
+      var self = this;
+
+      setTimeout(function() {
+        self.$alertModule.removeClass('collapsed');
+      }, 1000 );
+
+    },
+    
+    _closeAlert: function(event) {
+      var el = event.target, 
+          self = this;
+      
+      event.preventDefault();
+      self.$alertModule.addClass('collapsed');
+      
+      // after we close the alert lets remove it from the DOM
+      setTimeout(function() {
+        self.$alertModule.remove();
+      }, 1500 );
+    },
+
+    // init our bindings to show? // hide the message
+    _initBindings: function() {
+      var self = this;
+
+      self.$alertModule.on('click', 'button', $.proxy(self._closeAlert, self));
+    },
+
     init: function() {
-      console.log('init');
+      var self = this;
+
+      $(domReady);
+
+      // once the dom is ready we can fire our calls
+      function domReady() {
+        self._initBindings();
+        self._checkCookies();
+      }
     }
 
   }; // end AlertModule prototype
 
+  return module;
 });
--- src/packages/modules/cookie-policy/js/index.js
+++ src/packages/modules/cookie-policy/js/index.js
@@ -1,13 +1,13 @@
+/*jshint debug:true */
 
 define(function(require){
 
-  _AlertModule = require('modules/cookie-policy/alert-module');
+  var AlertModule = require('modules/cookie-policy/alert-module');
 
   // Initialize Modules that don't require additional configuration.
-  _AlertModule.init();
+  AlertModule.init();
 
-  // Return up a level if desired.
   return {
-    AlertModule: _AlertModule
+    AlertModule: AlertModule
   };
 });
