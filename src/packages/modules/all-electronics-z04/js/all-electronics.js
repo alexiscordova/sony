c84cce8237fedb48061b42111c3f17a6a445a28d
Mark Ernst <mernst@nerdery.com>
2013-04-26 10:29:20 -0500

    Adds js and image update functionality to all electronics module

---
+++ src/packages/modules/all-electronics-z04/js/all-electronics.js
@@ -0,0 +1,33 @@
+define(function(require){
+
+  'use strict';
+
+  var $ = require('jquery'),
+      bootstrap = require('bootstrap'),
+      iQ = require('iQ'),
+      enquire = require('enquire');
+
+  var module = {
+    init: function() {
+
+      var breakpoints = [ 767, 768, 979, 980 ];
+      var breakpointReactor = function( e ) {
+        iQ.update();
+      };
+
+      for( var i=0; i < breakpoints.length; i++ ) {
+        if( 0 === i ) {
+          /* log( "(max-width: " + breakpoints[ i ] + "px)" ); */
+          enquire.register( "(max-width: " + breakpoints[ i ] + "px)", breakpointReactor).listen();
+        } else {
+          /* log( "(min-width: " + ( breakpoints[ i-1 ] + 1 ) + "px) and (max-width: " + breakpoints[ i ] + "px)" ); */
+          enquire.register( "(min-width: " + ( breakpoints[ i-1 ] + 1 ) + "px) and (max-width: " + breakpoints[ i ] + "px)", breakpointReactor).listen();
+        }
+      }
+    }
+
+  };
+
+  return module;
+
+});
