94056192bec7f4e98e350b5860f95f196e5ca49d
Sam Carlton <sam.crltn@gmail.com>
2013-05-03 12:50:29 -0500

    Updates the country-region-selection module's header footer demo to no longer collapse the scrollers height on page load

--- src/packages/modules/country-region-selection-z10/js/index.js
+++ src/packages/modules/country-region-selection-z10/js/index.js
@@ -3,7 +3,9 @@ define(function(require){
   var CountryRegionSelection = require('modules/country-region-selection-z10/country-region-selection-z10');
 
   // Initialize Modules that don't require additional configuration.
-  CountryRegionSelection.init();
+  $(document).ready(function () {
+    CountryRegionSelection.init();
+  });
 
   // Return up a level if desired.
   return {
