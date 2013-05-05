28c5de45a97973cc7da8fea84daa7c03d5aee421
Sam Carlton <sam.crltn@gmail.com>
2013-05-05 17:33:41 -0500

    Updates light compare js to follow module template

--- src/packages/modules/light-compare-d9/js/light-compare-d9.js
+++ src/packages/modules/light-compare-d9/js/light-compare-d9.js
@@ -1,3 +1,11 @@
+// Light Compare Module.
+// ---------------------------------------
+//
+// * **Version:** 0.1
+// * **Modified:** 05/05/2013
+// * **Author:** Mark Ernst
+// * **Dependencies:** jQuery 1.7+, Bootstrap
+
 define(function(require){
 
   'use strict';
@@ -7,7 +15,7 @@ define(function(require){
       sonyStickyTabs = require('secondary/index').sonyStickyTabs;
 
   var module = {
-    init: function() {
+    'init': function() {
       $('.launch-modal').on('click', function(){
         $('#light-compare-modal').modal();
       });
