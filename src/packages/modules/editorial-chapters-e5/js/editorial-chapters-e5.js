125bcbb47fed64dbda483c400a6375c720e2634c
Kaleb White <info@cujo.jp>
2013-05-21 16:09:28 -0700

    fixed bug with tab nav 'bouncing' on interaction + was due to mobile devices not having a hover state, so borders were being removed and applied too soon

--- src/packages/modules/editorial-chapters-e5/js/editorial-chapters-e5.js
+++ src/packages/modules/editorial-chapters-e5/js/editorial-chapters-e5.js
@@ -264,9 +264,19 @@ define(function(require){
 
       // Sets the current active thumbnail
       setCurrentActiveThumb: function(){
-        var self = this;
-        self.$thumbNav.find('li').removeClass('active')
-          .eq( self.currentId ).addClass('active');
+        var self = this, 
+            $chapterTabs,
+            $currTab;
+
+        $chapterTabs = self.$thumbNav.find('li');
+        $currTab = $chapterTabs.eq( self.currentId );
+
+        // need to set a tmeout of 100ms so we can 
+        // fix a flicker bug on mobile devices
+        setTimeout(function() {
+          $chapterTabs.removeClass('active');
+          $currTab.addClass('active');
+        },100);       
       },
 
       initScroller: function(){
