f1949116a54dc8f179489ea467bb3da7983427e8
pete <pete@odopod.com>
2013-05-23 16:07:38 -0700

    fixes iphone video done state

--- src/packages/modules/primary-tout-p1-d7/js/primary-tout-p1-d7.js
+++ src/packages/modules/primary-tout-p1-d7/js/primary-tout-p1-d7.js
@@ -15,7 +15,7 @@ define(function(require){
     var $ = require('jquery'),
         // enquire = require('enquire'),
         iQ = require('iQ'),
-        // Settings = require('require/sony-global-settings'),
+        Settings = require('require/sony-global-settings'),
         Environment = require('require/sony-global-environment');
 
     var module = {
@@ -86,8 +86,10 @@ define(function(require){
         if(btn.length > 0){
           btn.on('click', function(e){
             e.preventDefault();
-            self.$el.find('.hero-image, .inner, .mobile-buttons-wrap').addClass('off-screen visuallyhidden');
-            self.$el.find('.submodule').eq($(this).data('submodule')).removeClass('off-screen visuallyhidden');
+            if(!Settings.isIPhone){
+              self.$el.find('.hero-image, .inner, .mobile-buttons-wrap').addClass('off-screen visuallyhidden');
+              self.$el.find('.submodule').eq($(this).data('submodule')).removeClass('off-screen visuallyhidden');
+            }
             //update for slideshow coming into view
             iQ.update();
             //play video if its a video button
