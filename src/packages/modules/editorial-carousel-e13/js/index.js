ccf73b3af3209a75b83b612666645e14fad2367d
pete <pete@odopod.com>
2013-05-22 17:33:42 -0700

    fixes carousel init / update sequence

--- src/packages/modules/editorial-carousel-e13/js/index.js
+++ src/packages/modules/editorial-carousel-e13/js/index.js
@@ -1,7 +1,7 @@
 
 define(function(require){
 
-  _EditorialCarouselE13 = require('modules/editorial-carousel-e13/editorial-carousel-e13');
+  var _EditorialCarouselE13 = require('modules/editorial-carousel-e13/editorial-carousel-e13');
 
   // Initialize Modules that don't require additional configuration.
   _EditorialCarouselE13.init();
