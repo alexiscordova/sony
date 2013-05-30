865ce27c729400178374eb919fda4a237f7dd409
Tyler Madison <tyler@odopod.com>
2013-05-30 15:26:25 -0700

    Fixed: SGFE-1041
    [CLIENT] E3 / all mobile browsers / can full thumbnail be shown for all mode layouts on mobile

--- src/packages/modules/pdp-slideshow-d4/js/pdp-slideshow-d4.js
+++ src/packages/modules/pdp-slideshow-d4/js/pdp-slideshow-d4.js
@@ -138,8 +138,8 @@ define(function(require) {
         self.$slideContainer.sonyCarousel({
           wrapper: '.pdp-slideshow-outer',
           slides: '.pdp-slideshow-slide',
-          looped: true,
-          jumping: true,
+          looped: !Settings.isPS3,
+          jumping: !Settings.isPS3,
           axis: 'x',
           dragThreshold: 2,
           paddles: true,
@@ -163,6 +163,10 @@ define(function(require) {
 
         self.currentId = currentIndex;
         self.setCurrentActiveThumb();
+
+        //shut off any players
+        
+
 
         setTimeout( iQ.update , 250 );
       },
