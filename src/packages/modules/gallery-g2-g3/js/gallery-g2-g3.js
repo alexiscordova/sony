alexiscordova/sony

commit c3fabbf801181c9d92a280e1717b206615b30a82

Used translateZ(0) to put range control handles and prices in their own layer (SGFE-365). Pretty sure there's absolutely nothing else that can improve the Arc S at this point. It's not reporting touch events when the handles are touched unless you are a sniper with your finger and hold it there.
Also removed commented out -moz-transition and -o-transition from mixins because they support the official syntax.

src/packages/modules/gallery-g2-g3/js/gallery-g2-g3.js
@@ -1760,6 +1760,9 @@ define(function(require){
       $minOutput = $output.find('.range-output-min .val'),
       $maxOutput = $output.find('.range-output-max .val'),
 
+      // Var to only call requestAnimationFrame once per frame
+      isTicking = false,
+
       delay = self.hasTouch ? 1000 : 750,
       method = self.hasTouch ? 'debounce' : 'throttle',
       debouncedFilter = $[ method ]( delay, function() {
@@ -1768,6 +1771,24 @@ define(function(require){
 
       function getPrice( percent ) {
         return Math.round( diff * (percent / 100) ) + MIN_PRICE;
+      }
+
+      function slid() {
+        if ( isTicking ) {
+          return;
+        }
+
+        var args = Array.prototype.slice.call( arguments, 0 );
+        isTicking = true;
+
+        // Only use rAF if it's native
+        if ( Modernizr.raf ) {
+          requestAnimationFrame( function updateWrap() {
+            update.apply( null, args );
+          });
+        } else {
+          update.apply( null, args );
+        }
       }
 
       // Range control update callback
@@ -1797,6 +1818,8 @@ define(function(require){
           // Throttle filtering (especially on touch)
           debouncedFilter();
         }
+
+        isTicking = false;
       }
 
       // Show what's happening with the range control
@@ -1837,7 +1860,7 @@ define(function(require){
       });
 
       // On handle slid, update. Register after initialized so it's not called during initialization
-      self.$rangeControl.on('slid.rangecontrol', update);
+      self.$rangeControl.on( 'slid.rangecontrol', slid );
 
       self.filterValues[ filterName ] = { min: true, max: true };
 

src/packages/common/css/_components/_range-control.scss
@@ -63,15 +63,20 @@ $handleHighlightColor: $buttonOrange;
       margin-left: -14px;
     }
 
-    // Make handles larger on touch
-    // .csstransforms.touch & {
-    //   @include scale(1.4);
-    //   @include box-shadow(0 0 0 3px $transparentBlack);
-    // }
+    // Make handles larger on touch. Arc S and Tablet S have severe problems getting touch events
+    .csstransforms.touch & {
+      @include transform( scale(1.4) translateZ(0) );
+      @include box-shadow(0 0 0 3px $transparentBlack);
+    }
+
+    // Promote to its own layer
+    .csstransforms & {
+      @include transform( translateZ(0) );
+    }
 
     .csstransforms &.grabbed,
     .csstransforms &:hover {
-      @include scale(1.7);
+      @include transform( scale(1.7) translateZ(0) );
       @include box-shadow(0 0 0 3px $transparentBlack);
     }
 
@@ -126,11 +131,11 @@ $handleHighlightColor: $buttonOrange;
 }
 
 .range-output-min {
-  @include translate(4px, 0);
+  @include transform( translate(4px, 0) translateZ(0) );
 }
 
 .range-output-max {
-  @include translate(-6px, 0);
+  @include transform( translate(-6px, 0) translateZ(0) );
 }
 
 .grabbing-min .range-output-min,

src/packages/common/js/require/sony-global-environment.js
@@ -301,7 +301,8 @@ define(function (require) {
         Settings.$html.removeClass('no-generatedcontent').addClass('generatedcontent');
       }
 
-      if ( Settings.isLTIE8 ) {
+      // IE7 getting false positive
+      if ( Modernizr.generatedcontent && Settings.isLTIE8 ) {
         Modernizr.generatedcontent = false;
         Settings.$html.removeClass('generatedcontent').addClass('no-generatedcontent');
       }

src/packages/modules/gallery-g2-g3/css/gallery-g2-g3.scss
@@ -474,7 +474,7 @@ $gifHeight: 60px;
     margin-bottom: 20px;
   }
 
-  // Fix webkit flicker bug
+  // Fix webkit flicker bug: promote buttons to their own layer
   .btn-square {
     @include translate3d(0,0,0);
   }

commit 41f5b8d58468fece5ef5901cd718968bffcad521

Fix logic error caused by c3fabbf. SGFE-1048

src/packages/modules/gallery-g2-g3/js/gallery-g2-g3.js
@@ -1814,7 +1814,7 @@ define(function(require){
       $maxOutput = $output.find('.range-output-max .val'),
 
       // Var to only call requestAnimationFrame once per frame
-      isTicking = false,
+      requestID = false,
 
       delay = self.hasTouch ? 1000 : 750,
       method = self.hasTouch ? 'debounce' : 'throttle',
@@ -1827,16 +1827,16 @@ define(function(require){
       }
 
       function slid() {
-        if ( isTicking ) {
-          return;
-        }
-
         var args = Array.prototype.slice.call( arguments, 0 );
-        isTicking = true;
 
         // Only use rAF if it's native
         if ( Modernizr.raf ) {
-          requestAnimationFrame( function updateWrap() {
+
+          // Make sure two call aren't executed in the same frame
+          if ( requestID ) {
+            cancelAnimationFrame( requestID );
+          }
+          requestID = requestAnimationFrame( function updateWrap() {
             update.apply( null, args );
           });
         } else {
@@ -1872,7 +1872,7 @@ define(function(require){
           debouncedFilter();
         }
 
-        isTicking = false;
+        requestID = false;
       }
 
       // Show what's happening with the range control
@@ -3893,7 +3893,7 @@ define(function(require){
   };
 
   // Event triggered when the next tab/pane is finished being shown
-  module.onGalleryTabShown = function( evt ) {
+  module.onGalleryTabShown = function() {
     // Only continue if this is a tab shown event.
     var $pane = $( this ),
         $galleries,