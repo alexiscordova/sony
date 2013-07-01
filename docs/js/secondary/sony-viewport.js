92f122ebb8f15932cd86d4f96d06dd372a43d906
Glen Cheney <glen@odopod.com>
2013-07-01 15:54:29 -0700

    Adding poorly cut assets to feature slideshow as well as static background.

--- docs/js/secondary/sony-viewport.js
+++ docs/js/secondary/sony-viewport.js
@@ -3,15 +3,11 @@
 //
 // * **Class:** Viewport
 // * **Version:** 1.0
-// * **Modified:** 06/26/2013
+// * **Modified:** 06/25/2013
 // * **Author:** Glen Cheney
 // * **Dependencies:** jQuery 1.7+, SONY Settings, throttle/debounce
 //
 // *Notes:*
-//
-// If you need to be notified when an element is scrolled into view, use this module.
-// This module keeps track of all elements that want to be watched and caches their offsets
-// and dimensions in order to keep scrolling as smooth as possible.
 //
 // *Example Usage:*
 //
@@ -41,112 +37,45 @@ define(function(require) {
 
       // The list of viewports to watch
       self.list = [];
-      self.lastScrollY = 0;
-      self.windowHeight = Settings.$window.height();
-      self.windowWidth = Settings.$window.width();
-      self.throttleTime = 250;
+      self.windowHeight = 0;
+      self.windowWidth = 0;
+
 
       self.onResize();
-      self.bindEvents();
-
-      // What's nice here is that rAF won't execute until the user is on this tab,
-      // so if they open the page in a new tab which they aren't looking at,
-      // this will execute when they come back to that tab
-      requestAnimationFrame(function() {
-        self.setScrollTop();
-        self.process();
-      });
+      self.listForEvents();
     },
 
-    bindEvents : function() {
+    listForEvents : function() {
       var self = this;
 
       // Listen for global resize
-      Environment.on('global:resizeDebounced.viewport', $.proxy( self.onResize, self ));
+      Environment.on('global:resizeDebounced', $.proxy( self.onResize, self ));
 
       // Throttle scrolling because it doesn't need to be super accurate
-      Settings.$window.on('scroll.viewport', $.throttle( self.throttleTime, $.proxy( self.onScroll, self ) ));
-
-      // When the universal nav is opened or closed, the trigger point needs adjustment along with scrollspy
-      Settings.$document.on('universal-nav-open-finished.viewport universal-nav-close-finished.viewport', function uNavChanged() {
-        setTimeout(function refreshWithDelay() {
-          self.onResize();
-        }, 0);
-      });
-
-      self.hasActiveHandlers = true;
-    },
-
-    unbindEvents : function() {
-      Environment.off('.viewport');
-      Settings.$window.off('.viewport');
-      Settings.$document.off('.viewport');
-
-      this.hasActiveHandlers = false;
-    },
-
-    maybeUnbindEvents : function() {
-      var self = this;
-
-      // Not currently watching anything, unbind events
-      if ( !self.list.length ) {
-        self.unbindEvents();
-      }
+      Settings.$window.on('scroll', $.throttle( 500, $.proxy( self.onScroll, self ) ));
     },
 
     add : function( options ) {
-      var self = this,
-          isThresholdPercentage = false;
+      var self = this;
 
-      // The whole point is to have a callback function.
-      // Don't do anything if it's not given
-      if ( !$.isFunction( options.callback ) ) {
-        throw 'Viewport.add :: No callback function provided to Viewport';
-      }
-
-      // Threshold can be a percentage. Parse it.
-      if ( (typeof options.threshold === 'string' && options.threshold.indexOf('%') > -1 ) ) {
-        isThresholdPercentage = true;
-        options.threshold = parseFloat( options.threshold ) / 100;
-
-      } else if ( options.threshold < 1 && options.threshold > 0 ) {
-        isThresholdPercentage = true;
-      }
-
-      options.isThresholdPercentage = isThresholdPercentage;
+      options.triggered = false;
       options.$element = $( options.element );
-
-      // Cache element's offsets and dimensions
-      options.offset = options.$element.offset();
+      options.elementOffset = options.$element.offset();
       options.height = options.$element.height();
       options.width = options.$element.width();
 
       self.list.push( options );
-
-      // Event handlers are removed if a callback is triggered and the
-      // watch list is empty. Because modules are instantiated asynchronously,
-      // another module could potentially add itself to the watch list when the events
-      // have been unbound.
-      // Check here if events have been unbound and bind them again if they have
-      if ( !self.hasActiveHandlers ) {
-        self.bindEvents();
-      }
-
-      requestAnimationFrame(function() {
-        self.process();
-      });
     },
 
     saveDimensions : function() {
       var self = this;
 
       $.each(self.list, function( i, listItem ) {
-        listItem.offset = listItem.$element.offset();
+        listItem.elementOffset = listItem.$element.offset();
         listItem.height = listItem.$element.height();
         listItem.width = listItem.$element.width();
       });
 
-      // self.documentHeight
       self.windowHeight = Settings.$window.height();
       self.windowWidth = Settings.$window.width();
     },
@@ -155,102 +84,29 @@ define(function(require) {
     onScroll : function() {
       var self = this;
 
-      // No point in doing anything if there aren't any viewports to watch
-      if ( !self.list.length ) {
-        return;
-      }
-
       // Save the new scroll top
-      self.setScrollTop();
+      self.lastScrollY = Settings.$window.scrollTop();
 
-      self.process();
+      self.processScroll();
     },
 
     // Debounced resize event
     onResize : function() {
       var self = this;
 
-      // No point in doing anything if there aren't any viewports to watch
-      if ( !self.list.length ) {
-        return;
-      }
-
       // Update offsets and width/height for each viewport item
       self.saveDimensions();
     },
 
-    isInViewport : function( viewport ) {
+    processScroll : function() {
       var self = this,
-          offset = viewport.offset,
-          threshold = viewport.threshold,
-          percentage = threshold,
-          st = self.lastScrollY,
-          isTopInView;
+          st = self.lastScrollY;
 
+      $.each(self.list, function( i, listItem ) {
 
-      if ( viewport.isThresholdPercentage ) {
-        threshold = 0;
-      }
-
-      // Other checks could be added here in the future
-      isTopInView = self.isTopInView( st, self.windowHeight, offset.top, viewport.height, threshold );
-
-      // If the top isn't in view with zero threshold,
-      // don't bother checking if it's at a percent of the window
-      if ( isTopInView && viewport.isThresholdPercentage ) {
-        isTopInView = self.isTopPastPercent( st, self.windowHeight, offset.top, viewport.height, percentage );
-      }
-
-      return isTopInView;
-    },
-
-    // If  the top of the element (plus the threshold) is past the viewport's top
-    // and the top of the element (plus the threshold) is not past the viewport's bottom.
-    // Then the top is in view.
-    isTopInView : function( viewportTop, viewportHeight, elementTop, elementHeight, threshold ) {
-      var viewportBottom = viewportTop + viewportHeight;
-      return (elementTop + threshold) >= viewportTop && (elementTop + threshold) < viewportBottom;
-    },
-
-    isTopPastPercent : function( viewportTop, viewportHeight, elementTop, elementHeight, percentage ) {
-      var viewportBottom = viewportTop + viewportHeight,
-          distFromViewportBottomToElementTop = viewportBottom - elementTop,
-          percentFromBottom = distFromViewportBottomToElementTop / viewportHeight;
-      return percentFromBottom >= percentage;
-    },
-
-    triggerCallback : function( index, listItem ) {
-      var self = this;
-
-      listItem.callback.call( listItem.element, listItem );
-
-      // No longer need to watch it, so remove from list
-      self.list.splice( index, 1 );
-
-      // If there are no more, unbind from scroll and resize events
-      self.maybeUnbindEvents();
-    },
-
-    setScrollTop : function() {
-      // Save the new scroll top
-      this.lastScrollY = Settings.$window.scrollTop();
-    },
-
-    process : function() {
-      var self = this,
-
-          // The list can possibly be modified mid loop,
-          // so the loop needs a copy of the variable which won't be modified
-          list = $.extend( [], self.list );
-
-      $.each(list, function( i, listItem ) {
-        var isInViewport = self.isInViewport( listItem );
-
-        if ( isInViewport ) {
-          self.triggerCallback( i, listItem );
-        }
       });
     }
+
   };
 
   Viewport.add = function( element, callback ) {
@@ -271,15 +127,17 @@ define(function(require) {
     }
 
     // Get defaults
-    options = $.extend( {}, Viewport.options, options, Viewport.settings );
+    $.extend( options, Viewport.options, options, Viewport.settings );
 
     return instance.add( options );
   };
 
+  // Options that could be customized per module instance
   Viewport.options = {
-    threshold: 200
+    offset: 200
   };
 
+  // These are not overridable when instantiating the module
   Viewport.settings = {
   };
 
