83005e7f5da99d6a02229c98c7822d02cf225776
George Pantazis <george@odopod.com>
2013-07-05 15:01:34 -0700

    Updates to SonyAudio

--- src/packages/modules/sony-audio-player/js/sony-audio-player.js
+++ src/packages/modules/sony-audio-player/js/sony-audio-player.js
@@ -29,6 +29,10 @@ define(function(require){
     var self = this;
 
     self.$el = $(element);
+    self.$tracks = self.$el.find('.track');
+    self.$nav = self.$el.find('nav');
+    self.$sourceSelect = self.$nav.find('select');
+
     self.init();
 
     log('SONY : SonyAudioPlayer : Initialized');
@@ -42,23 +46,56 @@ define(function(require){
 
       var self = this;
 
-      self.audio = new SonyAudio({
-        sources: {
-          'default': 'http://ia700200.us.archive.org/22/items/DebroySomersBand-31-40/DebroySomersAndHisBand-WestwardBound.mp3',
-          'low': 'http://ia700200.us.archive.org/22/items/DebroySomersBand-31-40/DebroySomersAndHisBand-WestwardBound.mp3'
-        }
+      self.currentTrack = 0;
+      self.setTrack(0);
+    },
+
+    setTrack: function(which) {
+
+      var self = this,
+          $track = self.$tracks.eq(which),
+          $sources = $track.find('.source'),
+          sourceList = {};
+
+      // START: The below should be moved into a trackInit method;
+      // we wouldn't want to call (most of) it multiple times.
+
+      self.$sourceSelect.empty();
+
+      $sources.each(function(a,b){
+
+        var key = $(this).text(),
+            value = $(this).find('a').attr('href');
+
+        sourceList[key] = value;
+
+        self.$sourceSelect.append('<option value="'+key+'">'+key+'</option>');
       });
 
-      self.$el.on('click', '.start-play', function() {
-        self.audio.play();
+      var track = new SonyAudio({
+        sources: sourceList
       });
 
-      self.$el.on('click', '.high-quality', function() {
-        self.audio.play('default');
+      self.$el.on('click', '.play', function(e) {
+        e.preventDefault();
+        track.play('default');
       });
 
-      self.$el.on('click', '.low-quality', function() {
-        self.audio.play('low');
+      self.$el.on('change', 'select', function(){
+        track.play($(this).val());
+      });
+
+      // END
+
+      self.setAlbumArt(which);
+    },
+
+    setAlbumArt: function(which) {
+
+      var self = this;
+
+      self.$el.css({
+        backgroundImage: 'url('+self.$tracks.eq(which).data('album-art')+')'
       });
     }
   };
