2d7ca46ddc5acb98508b9e4260a045ab10e01ccd
George Pantazis <george@odopod.com>
2013-07-09 12:19:34 -0700

    Fixing PromoClearAudio JS for build, Creating JSON for PDP Slideshow.

--- src/packages/modules/promo-clearaudio-b1/js/index.js
+++ src/packages/modules/promo-clearaudio-b1/js/index.js
@@ -1,7 +1,7 @@
 
 define(function(require){
 
-  var PromoClearAudio = require('modules/promo-clearaudio/promo-clearaudio');
+  var PromoClearAudio = require('modules/promo-clearaudio-b1/promo-clearaudio-b1');
 
   // Initialize Modules that don't require additional configuration.
   PromoClearAudio.init();
