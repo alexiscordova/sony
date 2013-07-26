cd50820920ef393d432c865198efea9233d0286a
Glen Cheney <cheney.glen@gmail.com>
2013-07-26 15:07:41 -0700

    Rename feature slideshow to -x00

--- src/packages/modules/feature-slideshow-b1-x00/js/index.js
+++ src/packages/modules/feature-slideshow-b1-x00/js/index.js
@@ -2,7 +2,7 @@
 define(function(require){
 
   var $ = require('jquery');
-  var FeatureSlideshow = require('modules/feature-slideshow-b1/feature-slideshow-b1');
+  var FeatureSlideshow = require('modules/feature-slideshow-b1-x00/feature-slideshow-b1-x00');
 
   // Initialize Modules that don't require additional configuration.
   $('.fs-module').each(function() {
