0a6f93c43655f99242baeeaf76c31e47d9f1bca2
Ryan Mauer <ryanm@14four.com>
2013-08-01 16:27:36 -0700

    Refactoring and cleanup.

--- src/packages/modules/exploded-view-b2-x00/js/exploded-view-b2-x00.js
+++ src/packages/modules/exploded-view-b2-x00/js/exploded-view-b2-x00.js
@@ -1,10 +1,10 @@
 // Demo Module
 // --------------------------------------------
 //
-// * **Class:** RecentlyViewed
+// * **Class:** ExplodedView
 // * **Version:** 1.0
-// * **Modified:** 04/18/2013
-// * **Author:** Your Name
+// * **Modified:** 08/01/2013
+// * **Author:** Ryan Mauer
 // * **Dependencies:** jQuery 1.7+ , Modernizr
 //
 // *Notes:*
@@ -58,55 +58,7 @@ define(function(require) {
       var self = this;
 
       self.setVars();
-
-      if( !self.hasTouch ) {
-
-        self.$el.on( 'mouseenter', 'a.cta', function( event ) {
-          log( 'Teasing' );
-          if( !self.isTeasing && !self.isExpanded ) {
-            self.isTeasing = true;
-            $( '.piece', self.$el ).addClass( 'quick' ).addClass( 'tease' );
-          }
-        });
-
-        self.$el.on( 'mouseleave', 'a.cta', function( event ) {
-          log( 'Leave Teasing' );
-          if( self.isTeasing && !self.isExpanded ) {
-            $( '.piece', self.$el ).removeClass( 'tease' );
-            self.isTeasing = false;
-          }
-        });
-
-      }
-
-      // Probably set some variables here
-      self.$el.on( 'click', 'a.cta', function( event ) {
-
-        event.preventDefault();
-
-        if( self.isExpanded ) {
-          $( '.callout', self.$el ).removeClass( 'in' );
-          $( '.detail', self.$el ).removeClass( 'in' );
-          $( '.piece', self.$el ).removeClass( 'exploded' );
-          $( '.cta', self.$el ).addClass( 'out' );
-          setTimeout( function() {
-            $( '.intro', self.$el ).removeClass( 'out' );
-            $( '.cta', self.$el ).removeClass( 'open' ).removeClass( 'out' );
-          }, 900);
-          self.isExpanded = false;
-        }
-        else {
-          $( '.cta', self.$el ).addClass( 'out' );
-          setTimeout( function() {
-            $( '.cta', self.$el ).addClass( 'open' ).removeClass( 'out' );
-            $( '.detail', self.$el ).addClass( 'in' );
-            $( '.callout', self.$el ).addClass( 'in' );
-          }, 800);
-          $( '.piece', self.$el ).removeClass( 'quick' ).addClass( 'exploded' ).removeClass( 'tease' );
-          $( '.intro', self.$el ).addClass( 'out' );
-          self.isExpanded = true;
-        }
-      } );
+      self.setupCTA();
 
       if ( Modernizr.mediaqueries ) {
         // These can be chained, like below
@@ -135,15 +87,23 @@ define(function(require) {
     setVars : function() {
       var self = this;
 
+      // State variables
       self.isExpanded = false;
       self.isTeasing = false;
+
+      // DOM elements
+      self.$cta     = $( '.cta', self.$el );
+      self.$pieces  = $( '.piece', self.$el );
+      self.$intro   = $( '.intro', self.$el );
+      self.$callout = $( '.callout', self.$el );
+      self.$detail  = $( '.detail', self.$el );
 
       // Modernizr vars
       self.hasTouch = Modernizr.touch;
 
     },
 
-    // Stubbed method. You don't have to use this
+    // Setup desktop.
     setupDesktop : function() {
       var self = this,
           wasMobile = self.isMobile;
@@ -156,7 +116,7 @@ define(function(require) {
       self.isMobile = false;
     },
 
-    // Stubbed method. You don't have to use this
+    // Setup mobile.
     setupMobile : function() {
       var self = this,
           wasDesktop = self.isDesktop;
@@ -167,6 +127,102 @@ define(function(require) {
 
       self.isDesktop = false;
       self.isMobile = true;
+    },
+
+    // Bind events on the CTA
+    setupCTA : function() {
+      var self = this;
+
+      // Hovering the CTA on non-touch devices displays the exploded elements in a "tease" state
+      // to entice the user to click
+      if( !self.hasTouch ) {
+
+        self.$el.on( 'mouseenter', 'a.cta', function( event ) {
+          self.expandTease();
+        });
+
+        self.$el.on( 'mouseleave', 'a.cta', function( event ) {
+          self.collapseTease();
+        });
+      }
+
+      // Expand and collapse the exploded elements based on clicks on the CTA
+      // and the current state
+      self.$el.on( 'click', 'a.cta', function( event ) {
+        event.preventDefault();
+        if( self.isExpanded ) {
+          self.collapse();
+        }
+        else {
+          self.expand();
+        }
+      } );
+
+    },
+
+    // Expand the exploded elements and transition in the other expanded state elements
+    expand : function() {
+      var self = this;
+
+      if( !self.isExpanded ) {
+        // not already expanded so expand
+        self.$cta.addClass( 'out' );
+        setTimeout( function() {
+          // wait before transitioning in the collapse CTA, detail box, and callout
+          self.$cta
+            .addClass( 'open' )
+            .removeClass( 'out' );
+          self.$detail.addClass( 'in' );
+          self.$callout.addClass( 'in' );
+        }, 800 );
+        self.$pieces
+          .removeClass( 'quick' )
+          .addClass( 'exploded' )
+          .removeClass( 'tease' );
+        self.$intro.addClass( 'out' );
+        self.isExpanded = true;
+      }
+
+    },
+
+    // Collapse the exploded elements and transition in the other collapsed state elements
+    collapse : function() {
+      var self = this;
+      // already expanded so collapse
+      self.$callout.removeClass( 'in' );
+      self.$detail.removeClass( 'in' );
+      self.$pieces.removeClass( 'exploded' );
+      self.$cta.addClass( 'out' );
+      setTimeout( function() {
+        // waith before transitioning in the intro and expand CTA
+        self.$intro.removeClass( 'out' );
+        self.$cta
+          .removeClass( 'open' )
+          .removeClass( 'out' );
+      }, 900 );
+      self.isExpanded = false;
+    },
+
+    // Expand the tease state if it is not already expanded and the module is not 
+    // already in the expanded state
+    expandTease : function() {
+      var self = this;
+      if( !self.isTeasing && !self.isExpanded ) {
+        self.isTeasing = true;
+        self.$pieces
+          .addClass( 'quick' )
+          .addClass( 'tease' );
+      }
+    },
+
+    // Collapse the tease state if it is not already collapsed and the module
+    // is not already in the expanded state
+    collapseTease : function() {
+      var self = this;
+      if( self.isTeasing && !self.isExpanded ) {
+        self.$pieces.removeClass( 'tease' );
+        self.isTeasing = false;
+      }
     },
 
     // Stubbed method. You don't have to use this
